6293b78e8c1b4f2c63330ca56d6571b558730117
Seb65789 <nati.seb.2002@gmail.com>
2025-04-13 15:04:55 +0200

    Data saved

--- Homework/Homework_1/src/split_dataset.py
+++ Homework/Homework_1/src/split_dataset.py
@@ -1,11 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Load the dataset
+#================================================================================================#
+
 print("Loading dataset...")
 
-# Load the dataset
 data = np.load("./Homework/Homework_1/src/intel_landscapes.npz")
-print(data)
 
 X_train = data['train_images']
 Y_train = data['train_labels']
@@ -19,8 +20,27 @@ Y_test = data['test_labels']
 plt.imshow(X_train[1]) 
 plt.savefig('./Homework/Homework_1/src/test_image.png')
 
-print(Y_train[1])
+print("Data Loaded")
 
-print("Number of train images :",len(X_train))
-print("Number of validation images :", len(X_val))
-print("Number of test images :", len(X_test))
+print("Number of train images equal to the excepted number ? ",len(X_train)==12630)
+print("Number of validation images equal to the excepted number ?", len(X_val)==1404)
+print("Number of test images equal to the excepted number ?", len(X_test)==3000)
+
+# Save the data
+#================================================================================================#
+
+print("Saving the data...")
+
+np.save("./Homework/Homework_1/src/data/X_train.npz",X_train)
+np.save("./Homework/Homework_1/src/data/X_val.npz",X_val)
+np.save("./Homework/Homework_1/src/data/X_test.npz",X_test)
+
+np.save("./Homework/Homework_1/src/data/Y_train.npz",Y_train)
+np.save("./Homework/Homework_1/src/data/Y_val.npz",Y_val)
+np.save("./Homework/Homework_1/src/data/Y_test.npz",Y_test)
+
+print("Data saved.")
+
+#================================================================================================#
+
+
