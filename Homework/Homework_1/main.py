c60f9128c9793075fec06cfaf37daf2e73ff0b5c
Seb65789 <nati.seb.2002@gmail.com>
2025-04-14 12:31:31 +0200

    Main Created and slip dataset.py deleted

---
+++ Homework/Homework_1/main.py
@@ -0,0 +1,22 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Load the dataset
+#================================================================================================#
+print("Loading dataset...")
+data = np.load("./Homework/Homework_1/src/intel_landscapes.npz")
+
+X_train = data['train_images']
+Y_train = data['train_labels']
+
+X_val = data['val_images']
+Y_val = data['val_labels']
+
+X_test = data['test_images']
+Y_test = data['test_labels']
+
+plt.imshow(X_train[0])
+plt.title(f"Label: {Y_train[0]}")
+plt.savefig("./Homework/Homework_1/test.png")
